2a8f567cea6b4dc1e2c4e63635a55e5a1f5e2a36
Philipp Langfeldt <p.langfeldt@live.de>
2023-08-22 10:36:10 +0200

    added 1.2

--- lab-sql-aggregation-and-transformation.sql
+++ lab-sql-aggregation-and-transformation.sql
@@ -2,18 +2,25 @@ USE sakila;
 
 # 1. As a movie rental company, we need to use SQL built-in functions to help us gain insights into our business operations:
 # 1.1 Determine the shortest and longest movie durations and name the values as max_duration and min_duration.
-SELECT MAX(length) AS max_duration, MIN(length) AS min_duration
-FROM sakila.film;
+SELECT
+	MAX(length) AS max_duration, MIN(length) AS min_duration
+FROM
+	sakila.film;
 
 # 1.2. Express the average movie duration in hours and minutes. Don't use decimals. Hint: look for floor and round functions.
-SELECT ROUND(AVG(length))
-FROM sakila.film;
+SELECT
+	FLOOR(AVG(length)/60) AS average_hours,
+    ROUND(AVG(length)%60) AS average_minutes
+FROM
+	sakila.film;
 
 # 2. We need to use SQL to help us gain insights into our business operations related to rental dates:
 # 2.1 Calculate the number of days that the company has been operating.
 # Hint: To do this, use the rental table, and the DATEDIFF() function to subtract the earliest date in the rental_date column from the latest date.
-SELECT DATEDIFF(MAX(rental_date), MIN(rental_date)) AS operating_days
-FROM sakila.rental;
+SELECT
+	DATEDIFF(MAX(rental_date), MIN(rental_date)) AS operating_days
+FROM
+	sakila.rental;
 
 # 2.2 Retrieve rental information and add two additional columns to show the month and weekday of the rental. Return 20 rows of results.
 SELECT
@@ -35,11 +42,11 @@ SELECT
     # Adding DAY_TYPE column using a conditional expression
     CASE
         WHEN DAYOFWEEK(rental_date) IN (1, 7) THEN 'weekend'
+        # date_format(rental_date, '%D') IN ('SATURDAY', 'SUNDAY') THEN... also possible
         ELSE 'workday'
     END AS DAY_TYPE
 FROM
-    sakila.rental
-LIMIT 20;
+    sakila.rental;
 
 # 3. We need to ensure that our customers can easily access information about our movie collection. To achieve this, retrieve the film titles and their rental duration.
 # If any rental duration value is NULL, replace it with the string 'Not Available'. Sort the results by the film title in ascending order.
